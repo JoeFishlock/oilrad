5c955748c083036bebf3d4f3ffb0db195a729ddc
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-04-18 15:44:24 +0100

    Make single layer model adhere to Model interface defined.
    Changing this to use a class interface will break the preliminary
    plotting scripts: albedo.py single_layer_solution.py and
    two_layer_albedo.py.

--- gulf/single_layer.py
+++ gulf/single_layer.py
@@ -16,137 +16,103 @@ from gulf.optics import (
     calculate_ice_oil_extinction_coefficient,
     calculate_ice_scattering_coefficient_from_Roche_2022,
 )
-
-########################
-#  Albedo calculation  #
-########################
+from dataclasses import dataclass
+from gulf.two_stream_model import AbstractModel
 
 
-def calculate_albedo(ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type: str):
-    """calculate spectral alebdo with no Fresnel reflection
-    wavelength in nm
-    ice thickness (m)
-    oil mass ratio (ng oil/g ice)
-    ice_type for scattering coefficient
+@dataclass
+class SingleLayerModel(AbstractModel):
     """
-    total_absorption = calculate_ice_oil_absorption_coefficient(
-        wavelength_in_nm, oil_mass_ratio
-    )
-    extinction_coefficient = calculate_ice_oil_extinction_coefficient(
-        wavelength_in_nm, oil_mass_ratio, ice_type
-    )
-    scattering = calculate_ice_scattering_coefficient_from_Roche_2022(ice_type)
-    optical_depth = extinction_coefficient * ice_thickness
-    return scattering / (
-        total_absorption
-        + scattering
-        + (extinction_coefficient / np.tanh(optical_depth))
-    )
+    For computational stability try represnting single layer solution in exponential
+    basis:
 
+    upwelling = A exp(+ext z) + B exp(-ext z)
+    downwelling = C exp(+ext z) + D exp(-ext z)
 
-def calculate_optically_thick_albedo(wavelength_in_nm, oil_mass_ratio, ice_type: str):
-    """calculate spectral alebdo for wavelength in nm in optically thick limit
-    oil mass ratio (ng oil/g ice)
-    ice_type for scattering coefficient
+    B = -A exp(-2 optical depth)
+    C = A/s
+    D = 1 - A/s
+
+    s is optically thick single layer albedo
+    optical depth = ice_thickness * extinction coefficient
+
+    Non dimensionalise by the incident shortwave at each wavelength so that
+    upwelling radiation -> upwelling * incident shortwave spectrum
     """
-    total_absorption = calculate_ice_oil_absorption_coefficient(
-        wavelength_in_nm, oil_mass_ratio
-    )
-    extinction_coefficient = calculate_ice_oil_extinction_coefficient(
-        wavelength_in_nm, oil_mass_ratio, ice_type
-    )
-    return (extinction_coefficient - total_absorption) / (
-        extinction_coefficient + total_absorption
-    )
 
+    oil_mass_ratio: float
+    ice_thickness: float
+    ice_type: str
 
-#######################################
-#  Upwelling / downwelling radiation  #
-#######################################
+    @property
+    def r(self):
+        return calculate_ice_scattering_coefficient_from_Roche_2022(self.ice_type)
 
-"""
-For computational stability try represnting single layer solution in exponential
-basis:
+    def k(self, L):
+        return calculate_ice_oil_absorption_coefficient(
+            L, oil_mass_ratio=self.oil_mass_ratio
+        )
 
-upwelling = A exp(+ext z) + B exp(-ext z)
-downwelling = C exp(+ext z) + D exp(-ext z)
+    def mu(self, L):
+        return calculate_ice_oil_extinction_coefficient(
+            L, oil_mass_ratio=self.oil_mass_ratio, ice_type=self.ice_type
+        )
 
-B = -A exp(-2 optical depth)
-C = A/s
-D = 1 - A/s
+    def s(self, L):
+        return (self.mu(L) - self.k(L)) / (self.mu(L) + self.k(L))
 
-s is optically thick single layer albedo
-optical depth = ice_thickness * extinction coefficient
+    def opt_depth(self, L):
+        return self.mu(L) * self.ice_thickness
 
-Non dimensionalise by the incident shortwave at each wavelength so that
-upwelling radiation -> upwelling * incident shortwave spectrum
-"""
+    def A(self, L):
+        return self.s(L) * (
+            1
+            / (
+                1
+                - np.exp(-2 * self.opt_depth(L))
+                * (
+                    (self.k(L) + self.r - self.mu(L))
+                    / (self.k(L) + self.r + self.mu(L))
+                )
+            )
+        )
 
+    def upwelling(self, z, L):
+        return self.A(L) * (
+            np.exp(self.mu(L) * z)
+            - np.exp(-self.mu(L) * z) / np.exp(2 * self.opt_depth(L))
+        )
 
-def calculate_A(ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type: str):
-    k = calculate_ice_oil_absorption_coefficient(wavelength_in_nm, oil_mass_ratio)
-    mu = calculate_ice_oil_extinction_coefficient(
-        wavelength_in_nm, oil_mass_ratio, ice_type
-    )
-    r = calculate_ice_scattering_coefficient_from_Roche_2022(ice_type)
-    optical_depth = mu * ice_thickness
-    s = (mu - k) / (mu + k)
+    def downwelling(self, z, L):
+        return (self.A(L) / self.s(L)) * np.exp(self.mu(L) * z) + (
+            1 - (self.A(L) / self.s(L))
+        ) * np.exp(-self.mu(L) * z)
 
-    return s * (1 / (1 - np.exp(-2 * optical_depth) * ((k + r - mu) / (k + r + mu))))
+    def net_radiation(self, z, L):
+        return self.downwelling(z, L) - self.upwelling(z, L)
 
+    def albedo(self, L):
+        """calculate spectral alebdo with no Fresnel reflection
+        wavelength in nm
+        ice thickness (m)
+        oil mass ratio (ng oil/g ice)
+        ice_type for scattering coefficient
+        """
+        return self.r / (self.k(L) + self.r + (self.mu(L) / np.tanh(self.opt_depth(L))))
 
-def calculate_ND_upwelling(
-    z, ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type: str
-):
-    """Calculate upwelling radiation dvided by shortwave incidence in ice
-    -ice_thickness < z < 0
-    """
-    A = calculate_A(ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type)
-    mu = calculate_ice_oil_extinction_coefficient(
-        wavelength_in_nm, oil_mass_ratio, ice_type
-    )
-    optical_depth = mu * ice_thickness
-    return A * (np.exp(mu * z) - np.exp(-mu * z) / np.exp(2 * optical_depth))
+    def transmittance(self, L):
+        return self.downwelling(-self.ice_thickness, L)
 
+    def heating(self, z, L):
+        """Use two stream ODEs dFnet/dz = k*(upwelling + downwelling)
 
-def calculate_ND_downwelling(
-    z, ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type: str
-):
-    """Calculate downwelling radiation dvided by shortwave incidence in ice
-    -ice_thickness < z < 0
-    """
-    A = calculate_A(ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type)
-    mu = calculate_ice_oil_extinction_coefficient(
-        wavelength_in_nm, oil_mass_ratio, ice_type
-    )
-    k = calculate_ice_oil_absorption_coefficient(wavelength_in_nm, oil_mass_ratio)
-    s = (mu - k) / (mu + k)
+        Need to multiply by incident shortwave to get dimensional value
+        """
+        return self.k(L) * (self.upwelling(z, L) + self.downwelling(z, L))
 
-    return (A / s) * np.exp(mu * z) + (1 - (A / s)) * np.exp(-mu * z)
-
-
-def calculate_ND_net_radiation(
-    z, ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type: str
-):
-    return calculate_ND_downwelling(
-        z, ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type
-    ) - calculate_ND_upwelling(
-        z, ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type
-    )
-
-
-def calculate_ND_heating(
-    z, ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type: str
-):
-    """Use two stream ODEs dFnet/dz = k*(upwelling + downwelling)
-
-    Need to multiply by incident shortwave to get dimensional value
-    """
-    k = calculate_ice_oil_absorption_coefficient(wavelength_in_nm, oil_mass_ratio)
-    upwelling = calculate_ND_upwelling(
-        z, ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type
-    )
-    downwelling = calculate_ND_downwelling(
-        z, ice_thickness, wavelength_in_nm, oil_mass_ratio, ice_type
-    )
-    return k * (upwelling + downwelling)
+    def optically_thick_albedo(self, L):
+        """calculate spectral alebdo for wavelength in nm in optically thick limit
+        oil mass ratio (ng oil/g ice)
+        ice_type for scattering coefficient
+        """
+        return self.s(L)
