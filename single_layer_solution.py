d35792299ac191667bd7aa4374d4071dd849d10a
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-04-18 16:12:21 +0100

    fix single layer solution script to use new model interface

--- single_layer_solution.py
+++ single_layer_solution.py
@@ -1,15 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import quad
-
-from gulf.single_layer import (
-    calculate_ND_downwelling,
-    calculate_ND_upwelling,
-    calculate_albedo,
-    calculate_ND_heating,
-    calculate_ND_net_radiation,
-)
-from gulf.optics import calculate_ice_oil_extinction_coefficient
+from gulf.two_stream_model import get_two_stream_model
 
 plt.style.use(["science", "nature", "grid"])
 
@@ -18,10 +9,11 @@ ICE_TYPE = "FYI"
 WAVELENGTH = 400
 OIL = 1000
 
-print(calculate_albedo(ICE_THICKNESS, WAVELENGTH, OIL, ICE_TYPE))
-print(
-    "optical_thickness: ",
-    calculate_ice_oil_extinction_coefficient(WAVELENGTH, OIL, ICE_TYPE) * ICE_THICKNESS,
+no_oil_model = get_two_stream_model(
+    "1L", oil_mass_ratio=0, ice_thickness=ICE_THICKNESS, ice_type=ICE_TYPE
+)
+oil_model = get_two_stream_model(
+    "1L", oil_mass_ratio=OIL, ice_thickness=ICE_THICKNESS, ice_type=ICE_TYPE
 )
 
 z = np.linspace(-ICE_THICKNESS, 0, 100)
@@ -31,43 +23,43 @@ plt.title(f"Wavelength {WAVELENGTH}nm irradiance in {ICE_TYPE} {ICE_THICKNESS}m 
 plt.xlabel("Planar spectral irradiance / incident solar irradiance")
 plt.ylabel("Depth (m)")
 plt.plot(
-    calculate_ND_upwelling(z, ICE_THICKNESS, WAVELENGTH, 0, ICE_TYPE),
+    no_oil_model.upwelling(z, WAVELENGTH),
     z,
     "b",
     label="upwelling no oil",
 )
 plt.plot(
-    calculate_ND_upwelling(z, ICE_THICKNESS, WAVELENGTH, OIL, ICE_TYPE),
+    oil_model.upwelling(z, WAVELENGTH),
     z,
     "r",
     label=f"upwelling {OIL}ng oil/g ice",
 )
 plt.plot(
-    calculate_ND_downwelling(z, ICE_THICKNESS, WAVELENGTH, 0, ICE_TYPE),
+    no_oil_model.downwelling(z, WAVELENGTH),
     z,
     "b--",
     label="downwelling no oil",
 )
 plt.plot(
-    calculate_ND_downwelling(z, ICE_THICKNESS, WAVELENGTH, OIL, ICE_TYPE),
+    oil_model.downwelling(z, WAVELENGTH),
     z,
     "r--",
     label=f"downwelling {OIL}ng oil/g ice",
 )
 plt.plot(
-    calculate_ND_net_radiation(z, ICE_THICKNESS, WAVELENGTH, 0, ICE_TYPE),
+    no_oil_model.net_radiation(z, WAVELENGTH),
     z,
     "b:",
     label="net rad no oil",
 )
 plt.plot(
-    calculate_ND_net_radiation(z, ICE_THICKNESS, WAVELENGTH, OIL, ICE_TYPE),
+    oil_model.net_radiation(z, WAVELENGTH),
     z,
     "r:",
     label=f"net rad {OIL}ng oil/g ice",
 )
 plt.legend()
-plt.savefig("figures/radiation_streams.pdf")
+plt.savefig("figures/single_layer/radiation_streams.pdf")
 plt.close()
 
 """This figure shows the radiative heating for several oil concentrations
@@ -79,13 +71,19 @@ plt.title(f"Radiative heating at {WAVELENGTH}nm in {ICE_TYPE} {ICE_THICKNESS}m t
 plt.xlabel("Radiative heating / incident solar irradiance")
 plt.ylabel("Depth (m)")
 for oil_mass_ratio in [0, 1, 10, 100, 1000]:
+    model = get_two_stream_model(
+        "1L",
+        oil_mass_ratio=oil_mass_ratio,
+        ice_thickness=ICE_THICKNESS,
+        ice_type=ICE_TYPE,
+    )
     plt.plot(
-        calculate_ND_heating(z, ICE_THICKNESS, WAVELENGTH, oil_mass_ratio, ICE_TYPE),
+        model.heating(z, WAVELENGTH),
         z,
         label=f"heating {oil_mass_ratio} ng oil/g ice",
     )
 plt.legend()
-plt.savefig("figures/radiative_heating.pdf")
+plt.savefig("figures/single_layer/radiative_heating.pdf")
 plt.close()
 
 """This figure shows the amount of shortwave radiation absorbed by the ice is many times
@@ -96,44 +94,19 @@ plt.title(
 )
 plt.xlabel("Radiative heating / radiative heating no oil (%)")
 plt.ylabel("Depth (m)")
-base_heating = calculate_ND_heating(z, ICE_THICKNESS, WAVELENGTH, 0, ICE_TYPE)
+base_heating = no_oil_model.heating(z, WAVELENGTH)
 for oil_mass_ratio in [0, 1, 10, 100]:
+    model = get_two_stream_model(
+        "1L",
+        oil_mass_ratio=oil_mass_ratio,
+        ice_thickness=ICE_THICKNESS,
+        ice_type=ICE_TYPE,
+    )
     plt.plot(
-        100
-        * (
-            calculate_ND_heating(z, ICE_THICKNESS, WAVELENGTH, oil_mass_ratio, ICE_TYPE)
-            - base_heating
-        )
-        / base_heating,
+        100 * (model.heating(z, WAVELENGTH) - base_heating) / base_heating,
         z,
         label=f"{oil_mass_ratio} ng oil/g ice",
     )
 plt.legend()
-plt.savefig("figures/radiative_heating_change.pdf")
-plt.close()
-
-plt.figure(figsize=(4, 3))
-plt.title(
-    f"depth integrated radiative heating spectrum in {ICE_TYPE} {ICE_THICKNESS}m thick"
-)
-plt.ylabel("depth integrated radiative heating / incident solar irradiance spectrum")
-plt.xlabel("wavelength (nm)")
-spectrum = np.linspace(350, 750, 1000)
-for oil_mass_ratio in [0, 1, 10, 100, 1000]:
-    broadband_heating = []
-    for wavelength in spectrum:
-        radiative_heating = lambda z: calculate_ND_heating(
-            z, ICE_THICKNESS, wavelength, oil_mass_ratio, ICE_TYPE
-        )
-        integrated_radiative_heating, _ = quad(radiative_heating, -ICE_THICKNESS, 0)
-        broadband_heating.append(integrated_radiative_heating)
-
-    plt.plot(
-        spectrum,
-        broadband_heating,
-        label=f"{oil_mass_ratio} ng oil/g ice",
-    )
-plt.yscale("log")
-plt.legend()
-plt.savefig("figures/radiative_heating_depth_integrated.pdf")
+plt.savefig("figures/single_layer/radiative_heating_change.pdf")
 plt.close()
