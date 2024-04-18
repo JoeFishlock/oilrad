4b6c11d9e602df20fa0c47ba46276a07c1be3812
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-04-18 13:12:31 +0100

    Add a script to test effect of a noisey oil profile.
    It is likely that the oil mass ratio depth profile from a coupled model
    would be noisey and on a coarse grid. So simulate this and interpolate
    to pass to infinite layer model to see if it is an issue for the solver.
    
    Upshot is the irradaince profiles remain smooth but the radiative
    heating output becomes noisey reflecting the noisey input absorption
    which should be ok.

---
+++ interpolated_oil_test.py
@@ -0,0 +1,50 @@
+"""This script creates a noisy vertical oil mass ratio profile interpolated on a 
+fairly coarse grid as may be the case in a sea ice model. It then constructs a smooth
+version analytically and plots the solution to the infinite layer two stream model 
+for these two cases and a case with no oil.
+
+The conclusion is that the radiative heating term will be jagged like the input oil
+mass ratio profile bu the upwelling and downwelling irrradiances are smoothed.
+This shouldn't matter as this heating will undergo thermal diffusion and so temperature
+variations will be smoothed"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from gulf.infinite_layer import InfiniteLayerModel
+
+H = 2
+ICE_TYPE = "FYI"
+
+sample_depths = np.linspace(-H, 0, 25)
+sample_oil = np.maximum(
+    0,
+    (1000 * (sample_depths + H) ** 2 / 4)
+    * (1 + 2 * np.sin(np.pi * 500 * sample_depths) * np.sin(1000 * sample_depths)),
+)
+plt.figure()
+plt.plot(sample_oil, sample_depths)
+plt.show()
+plt.close()
+interpolated_oil_func = lambda z: np.interp(z, sample_depths, sample_oil)
+interpolated_oil_model = InfiniteLayerModel(interpolated_oil_func, H, ICE_TYPE)
+
+smooth_oil_func = lambda z: 1000 * (z + H) ** 2 / 4
+smooth_oil_model = InfiniteLayerModel(smooth_oil_func, H, ICE_TYPE)
+
+base = InfiniteLayerModel(lambda z: 0, H, ICE_TYPE)
+
+z = np.linspace(-H, 0, 1000)
+
+plt.figure()
+for model in [interpolated_oil_model, base, smooth_oil_model]:
+    plt.plot(model.downwelling(z, 350), z)
+    plt.plot(model.upwelling(z, 350), z, ls="--")
+plt.show()
+plt.close()
+
+plt.figure()
+for model in [interpolated_oil_model, base, smooth_oil_model]:
+    plt.plot(model.heating(z, 350), z)
+
+plt.show()
+plt.close()
