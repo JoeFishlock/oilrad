744452e53e7e62670e67744012d555afd436aef7
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-09-18 18:29:49 +0100

    Move irradiance classes to own file

--- oilrad/infinite_layer.py
+++ oilrad/infinite_layer.py
@@ -6,12 +6,12 @@ from dataclasses import dataclass
 from typing import Callable
 import numpy as np
 from numpy.typing import NDArray
-from scipy.integrate import solve_bvp, trapezoid
+from scipy.integrate import solve_bvp
 from .optics import (
     calculate_ice_oil_absorption_coefficient,
     calculate_ice_scattering_coefficient_from_Roche_2022,
 )
-from .black_body import normalised_black_body_spectrum
+from .irradiance import SpectralIrradiance
 
 
 @dataclass(frozen=True)
@@ -21,70 +21,6 @@ class InfiniteLayerModel:
     oil_mass_ratio: Callable[[NDArray], NDArray]
     ice_type: str
     median_droplet_radius_in_microns: float
-
-
-@dataclass(frozen=True)
-class SpectralIrradiance:
-    """vertical grid z specified in dimensional units (m)
-    discretised wavelengths are in nm and define the SW range
-    upwelling and downwelling irradiances are non-dimensional and need to be multiplied
-    by the incident radiation spectrum to regain dimensions.
-    """
-
-    z: NDArray
-    wavelengths: NDArray
-    upwelling: NDArray
-    downwelling: NDArray
-
-    @property
-    def net_irradiance(self) -> NDArray:
-        return self.downwelling - self.upwelling
-
-    @property
-    def albedo(self) -> NDArray:
-        return self.upwelling[-1, :]
-
-    @property
-    def transmittance(self) -> NDArray:
-        return self.downwelling[0, :]
-
-
-@dataclass(frozen=True)
-class Irradiance:
-    """vertical grid z specified in dimensional units (m)
-    upwelling and downwelling irradiances are non-dimensional and need to be multiplied
-    by the incident integrated SW radiation to regain dimensions.
-    """
-
-    z: NDArray
-    upwelling: NDArray
-    downwelling: NDArray
-
-    @property
-    def net_irradiance(self) -> NDArray:
-        return self.downwelling - self.upwelling
-
-    @property
-    def albedo(self) -> NDArray:
-        return self.upwelling[-1]
-
-    @property
-    def transmittance(self) -> NDArray:
-        return self.downwelling[0]
-
-
-def integrate_over_SW(spectral_irradiance: SpectralIrradiance) -> Irradiance:
-    """integrate over the SW spectrum gven as part of the SpectralIrradiance object
-    weighted by the normalised black body spectrum over this range"""
-    wavelengths = spectral_irradiance.wavelengths
-    integrate = lambda irradiance: trapezoid(
-        irradiance * normalised_black_body_spectrum(wavelengths), wavelengths, axis=1
-    )
-    integrated_upwelling = integrate(spectral_irradiance.upwelling)
-    integrated_downwelling = integrate(spectral_irradiance.downwelling)
-    return Irradiance(
-        spectral_irradiance.z, integrated_upwelling, integrated_downwelling
-    )
 
 
 def _get_ODE_fun(
