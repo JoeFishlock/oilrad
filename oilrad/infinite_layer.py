65495450e2aef597f20e931b6cd609ad3b4f7b8a
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-09-18 18:21:48 +0100

    change interface of inifinite layer model to maximise efficiency.
    The parameters for solution are now specified in the InfiniteLayerModel
    object which now takes the dimensional discretised grid to evaluate the
    solution on as well as the discretised points in wavelength space.
    
    The model is solved and returns a SpectralIrradiance object which
    provides irradiances in a 2D array of vertical coordinate and
    wavelength.
    
    I've also written a function to integrate this object in wavelength
    space and return an Irradiance object with the total integrated
    irradiances.

--- oilrad/infinite_layer.py
+++ oilrad/infinite_layer.py
@@ -1,68 +1,137 @@
-"""Solve the model with continuously varying optical parameters"""
+"""Solve two stream radiation model in layer of ice with continuously varying
+vertical profile of mass concentration of oil
+"""
 
+from dataclasses import dataclass
+from typing import Callable
 import numpy as np
-from oilrad.optics import (
+from numpy.typing import NDArray
+from scipy.integrate import solve_bvp, trapezoid
+from .optics import (
     calculate_ice_oil_absorption_coefficient,
     calculate_ice_scattering_coefficient_from_Roche_2022,
 )
-from oilrad.abstract_model import AbstractModel
-from dataclasses import dataclass
-from typing import Callable
-from scipy.integrate import solve_bvp
+from .black_body import normalised_black_body_spectrum
 
 
-@dataclass
-class InfiniteLayerModel(AbstractModel):
-    """F = [upwelling(z, L), downwelling(z, L)]"""
-
-    oil_mass_ratio: Callable[[float], float]
-    ice_thickness: float
+@dataclass(frozen=True)
+class InfiniteLayerModel:
+    z: NDArray
+    wavelengths: NDArray
+    oil_mass_ratio: Callable[[NDArray], NDArray]
     ice_type: str
     median_droplet_radius_in_microns: float
 
-    @property
-    def r(self):
-        return calculate_ice_scattering_coefficient_from_Roche_2022(self.ice_type)
 
-    def k(self, z, L):
+@dataclass(frozen=True)
+class SpectralIrradiance:
+    """vertical grid z specified in dimensional units (m)
+    discretised wavelengths are in nm and define the SW range
+    upwelling and downwelling irradiances are non-dimensional and need to be multiplied
+    by the incident radiation spectrum to regain dimensions.
+    """
+
+    z: NDArray
+    wavelengths: NDArray
+    upwelling: NDArray
+    downwelling: NDArray
+
+    @property
+    def net_irradiance(self) -> NDArray:
+        return self.downwelling - self.upwelling
+
+    @property
+    def albedo(self) -> NDArray:
+        return self.upwelling[-1, :]
+
+    @property
+    def transmittance(self) -> NDArray:
+        return self.downwelling[0, :]
+
+
+@dataclass(frozen=True)
+class Irradiance:
+    """vertical grid z specified in dimensional units (m)
+    upwelling and downwelling irradiances are non-dimensional and need to be multiplied
+    by the incident integrated SW radiation to regain dimensions.
+    """
+
+    z: NDArray
+    upwelling: NDArray
+    downwelling: NDArray
+
+    @property
+    def net_irradiance(self) -> NDArray:
+        return self.downwelling - self.upwelling
+
+    @property
+    def albedo(self) -> NDArray:
+        return self.upwelling[-1]
+
+    @property
+    def transmittance(self) -> NDArray:
+        return self.downwelling[0]
+
+
+def integrate_over_SW(spectral_irradiance: SpectralIrradiance) -> Irradiance:
+    """integrate over the SW spectrum gven as part of the SpectralIrradiance object
+    weighted by the normalised black body spectrum over this range"""
+    wavelengths = spectral_irradiance.wavelengths
+    integrate = lambda irradiance: trapezoid(
+        irradiance * normalised_black_body_spectrum(wavelengths), wavelengths, axis=1
+    )
+    integrated_upwelling = integrate(spectral_irradiance.upwelling)
+    integrated_downwelling = integrate(spectral_irradiance.downwelling)
+    return Irradiance(
+        spectral_irradiance.z, integrated_upwelling, integrated_downwelling
+    )
+
+
+def _get_ODE_fun(
+    model: InfiniteLayerModel, wavelength: float
+) -> Callable[[NDArray, NDArray], NDArray]:
+    r = calculate_ice_scattering_coefficient_from_Roche_2022(model.ice_type)
+
+    def k(z: NDArray) -> NDArray:
         return calculate_ice_oil_absorption_coefficient(
-            L,
-            oil_mass_ratio=self.oil_mass_ratio(z),
-            droplet_radius_in_microns=self.median_droplet_radius_in_microns,
+            wavelength,
+            oil_mass_ratio=model.oil_mass_ratio(z),
+            droplet_radius_in_microns=model.median_droplet_radius_in_microns,
         )
 
-    def _ODE_fun(self, z, F, L):
-        upwelling_part = -(self.k(z, L) + self.r) * F[0] + self.r * F[1]
-        downwelling_part = (self.k(z, L) + self.r) * F[1] - self.r * F[0]
+    def _ODE_fun(z: NDArray, F: NDArray) -> NDArray:
+        """F = [upwelling(z), downwelling(z)]"""
+        upwelling_part = -(k(z) + r) * F[0] + r * F[1]
+        downwelling_part = (k(z) + r) * F[1] - r * F[0]
         return np.vstack((upwelling_part, downwelling_part))
 
-    def _BCs(self, F_bottom, F_top):
-        """Doesn't depend on wavelength"""
-        return np.array([F_top[1] - 1, F_bottom[0]])
+    return _ODE_fun
 
-    def _get_system_solution(self, L):
-        solution = solve_bvp(
-            lambda z, F: self._ODE_fun(z, F, L=L),
-            self._BCs,
-            np.linspace(-self.ice_thickness, 0, 5),
-            np.zeros((2, 5)),
-            max_nodes=6000,
-        )
-        if not solution.success:
-            raise RuntimeError(f"{solution.message}")
-        return solution.sol
 
-    def upwelling(self, z, L):
-        return self._get_system_solution(L)(z)[0]
+def _BCs(F_bottom, F_top):
+    """Doesn't depend on wavelength"""
+    return np.array([F_top[1] - 1, F_bottom[0]])
 
-    def downwelling(self, z, L):
-        return self._get_system_solution(L)(z)[1]
 
-    def albedo(self, L):
-        return np.vectorize(self.upwelling)(0, L)
+def _solve_at_given_wavelength(model, wavelength: float) -> tuple[NDArray, NDArray]:
+    fun = _get_ODE_fun(model, wavelength)
+    solution = solve_bvp(
+        fun,
+        _BCs,
+        np.linspace(model.z[0], model.z[-1], 5),
+        np.zeros((2, 5)),
+        max_nodes=6000,
+    )
+    if not solution.success:
+        raise RuntimeError(f"{solution.message}")
+    return solution.sol(model.z)[0], solution.sol(model.z)[1]
 
-    def transmittance(self, L):
-        return np.vectorize(self.downwelling)(-self.ice_thickness, L)
 
-    def heating(self, z, L):
-        return self.k(z, L) * (self.upwelling(z, L) + self.downwelling(z, L))
+def solve(model: InfiniteLayerModel) -> SpectralIrradiance:
+    upwelling = np.empty((model.z.size, model.wavelengths.size))
+    downwelling = np.empty((model.z.size, model.wavelengths.size))
+    for i, wavelength in enumerate(model.wavelengths):
+        col_upwelling, col_downwelling = _solve_at_given_wavelength(model, wavelength)
+        upwelling[:, i] = col_upwelling
+        downwelling[:, i] = col_downwelling
+    return SpectralIrradiance(model.z, model.wavelengths, upwelling, downwelling)
