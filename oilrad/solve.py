c943320cfe37dde21a694fae1b2815b424a04723
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-09-27 14:48:51 +0100

    Add fast_solve option to infinite layer model.
    This requires specifying a wavelength cutoff. For short wavelengths run
    the ode solver. But for longer wavelengths approximate the solution
    as all absorbed within the first grid point. This cutoff therefore
    depends on the grid resolution the solution is requested on and the
    extinction coefficient in the domain.
    
    For resolutons of around 1cm a cutoff of 1500nm seems appropriate

--- oilrad/solve.py
+++ oilrad/solve.py
@@ -21,13 +21,29 @@ def solve_two_stream_model(
             for i, wavelength in enumerate(model.wavelengths):
                 upwelling[:, i] = model.upwelling(model.z, wavelength)
                 downwelling[:, i] = model.downwelling(model.z, wavelength)
-        case InfiniteLayerModel():
+        case InfiniteLayerModel(fast_solve=False):
             for i, wavelength in enumerate(model.wavelengths):
                 col_upwelling, col_downwelling = solve_at_given_wavelength(
                     model, wavelength
                 )
                 upwelling[:, i] = col_upwelling
                 downwelling[:, i] = col_downwelling
+        case InfiniteLayerModel(fast_solve=True):
+            cut_off_index = (
+                np.argmin(np.abs(model.wavelengths - model.wavelength_cutoff)) + 1
+            )
+            is_surface = np.s_[cut_off_index:]
+            is_interior = np.s_[:cut_off_index]
+            for i, wavelength in enumerate(model.wavelengths[is_interior]):
+                col_upwelling, col_downwelling = solve_at_given_wavelength(
+                    model, wavelength
+                )
+                upwelling[:, i] = col_upwelling
+                downwelling[:, i] = col_downwelling
+
+            upwelling[:, is_surface] = 0
+            downwelling[:, is_surface] = 0
+            downwelling[-1, is_surface] = 1
         case _:
             raise NotImplementedError
     return SpectralIrradiance(model.z, model.wavelengths, upwelling, downwelling)
