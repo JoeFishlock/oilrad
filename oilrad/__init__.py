cf6a89a423fbece6e9eb148e7e08b79d28e9570e
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-11-04 13:39:15 +0000

    Bump version number 0.9.0 -> 1.0.0

--- oilrad/__init__.py
+++ oilrad/__init__.py
@@ -1,5 +1,5 @@
 """Two-stream shortwave radiative transfer model for sea ice containing oil droplets."""
-__version__ = "0.9.0"
+__version__ = "1.0.0"
 
 from .irradiance import SpectralIrradiance, Irradiance, integrate_over_SW
 from .spectra import BlackBodySpectrum
