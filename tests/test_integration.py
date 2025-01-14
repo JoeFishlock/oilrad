1ed3888824c02856a1a18ccbc0c32faf9a5acef2
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2025-01-14 17:41:33 +0000

    Add test case for six band model

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,7 +11,7 @@ WAVELENGTHS = np.geomspace(350, 3000, 50)
 ICE_SCATTERING_COEFFICIENT = 1.5
 
 # set up a variety of model configurations to test
-only_ice = oi.InfiniteLayerModel(
+only_ice = oi.CtsWavelengthModel(
     z=Z,
     wavelengths=WAVELENGTHS,
     oil_mass_ratio=np.full_like(Z, 0),
@@ -20,7 +20,7 @@ only_ice = oi.InfiniteLayerModel(
     liquid_fraction=np.full_like(Z, 0),
 )
 
-only_water = oi.InfiniteLayerModel(
+only_water = oi.CtsWavelengthModel(
     z=Z,
     wavelengths=WAVELENGTHS,
     oil_mass_ratio=np.full_like(Z, 0),
@@ -29,7 +29,7 @@ only_water = oi.InfiniteLayerModel(
     liquid_fraction=np.full_like(Z, 1),
 )
 
-no_oil = oi.InfiniteLayerModel(
+no_oil = oi.CtsWavelengthModel(
     z=Z,
     wavelengths=WAVELENGTHS,
     oil_mass_ratio=np.full_like(Z, 0),
@@ -37,7 +37,7 @@ no_oil = oi.InfiniteLayerModel(
     median_droplet_radius_in_microns=0.5,
     liquid_fraction=np.where(Z >= -ICE_DEPTH, 0, 1),
 )
-oil_1000 = oi.InfiniteLayerModel(
+oil_1000 = oi.CtsWavelengthModel(
     z=Z,
     wavelengths=WAVELENGTHS,
     oil_mass_ratio=np.full_like(Z, 1000),
@@ -45,7 +45,7 @@ oil_1000 = oi.InfiniteLayerModel(
     median_droplet_radius_in_microns=0.5,
     liquid_fraction=np.where(Z >= -ICE_DEPTH, 0, 1),
 )
-oil_1000_enhanced = oi.InfiniteLayerModel(
+oil_1000_enhanced = oi.CtsWavelengthModel(
     z=Z,
     wavelengths=WAVELENGTHS,
     oil_mass_ratio=np.full_like(Z, 1000),
@@ -59,7 +59,7 @@ oil_1000_enhanced = oi.InfiniteLayerModel(
 @pytest.mark.parametrize(
     "model", [only_ice, only_water, no_oil, oil_1000, oil_1000_enhanced]
 )
-def test_solve(model) -> None:
+def test_cts_wavelength_model(model) -> None:
     """test that solving the two-stream model and integrating the result over the
     blackbody spectrum does not raise an error"""
     oi.integrate_over_SW(
@@ -67,10 +67,29 @@ def test_solve(model) -> None:
     )
 
 
-def test_fast_solve() -> None:
+def test_six_band_model() -> None:
+    """test solving the six band model and integrating doesn't throw an error"""
+    z = np.linspace(-1, 0, 1000)
+    model = oi.SixBandModel(
+        z,
+        np.full_like(z, 1000),
+        ice_scattering_coefficient=800 * 0.15 * 0.75144 * 2,
+        median_droplet_radius_in_microns=0.05,
+        absorption_enhancement_factor=2,
+        snow_depth=0.2,
+        snow_spectral_albedos=oi.SNOW_ALBEDOS["light2022"],
+        snow_extinction_coefficients=oi.SNOW_EXTINCTION_COEFFICIENTS["lebrun2023"],
+        SSL_depth=0.04,
+        SSL_spectral_albedos=oi.SSL_ALBEDOS["light2022"],
+        SSL_extinction_coefficients=oi.SSL_EXTINCTION_COEFFICIENTS["perovich1990"],
+    )
+    oi.integrate_over_SW(oi.solve_two_stream_model(model))
+
+
+def test_cts_wavelength_fast_solve() -> None:
     """Test that the fast solver agrees with the full solver below the wavelength cutoff"""
     WAVELENGTH_CUTOFF = 1200
-    full_model = oi.InfiniteLayerModel(
+    full_model = oi.CtsWavelengthModel(
         z=Z,
         wavelengths=WAVELENGTHS,
         oil_mass_ratio=np.full_like(Z, 0),
@@ -78,7 +97,7 @@ def test_fast_solve() -> None:
         median_droplet_radius_in_microns=0.5,
         liquid_fraction=np.where(Z >= -ICE_DEPTH, 0, 1),
     )
-    fast_model = oi.InfiniteLayerModel(
+    fast_model = oi.CtsWavelengthModel(
         z=Z,
         wavelengths=WAVELENGTHS,
         oil_mass_ratio=np.full_like(Z, 0),
